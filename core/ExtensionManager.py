a7b091b516b045abe5ad3b608b62fd402074f892
Lapis0875 <lapis0875@gmail.com>
2020-08-27 22:24:15 +0900

    fix: Fix some invalid if-conditions

--- core/ExtensionManager.py
+++ core/ExtensionManager.py
@@ -1,8 +1,7 @@
-from typing import Type, Dict, List, Tuple, Union, overload, Callable, NoReturn
+from typing import Type, Dict, List, Tuple, Union, overload, Callable, NoReturn, Any, Optional
 from discord.ext.commands import Cog
 from discord.ext.commands.errors import *
 from .exceptions import BadExtArguments
-
 
 EXT_CONFIG = Dict[str, Union[str, Dict[str, List[Dict[str, str]]]]]
 EXT_MAP = Dict[str, Dict[str, str]]
@@ -40,7 +39,8 @@ class ExtensionManager:
                 ext_map[category][ext["ext_name"]] = self.convert_dir(ext_path)
 
         import json
-        print("[ExtensionManager.map] mapped extension_config :\n"+ json.dumps(obj=ext_map, indent=4, ensure_ascii=False))
+        print("[ExtensionManager.map] mapped extension_config :\n" + json.dumps(obj=ext_map, indent=4,
+                                                                                ensure_ascii=False))
         return ext_map
 
     def _validate_config(self, extensions_config: EXT_CONFIG) -> bool:
@@ -57,12 +57,14 @@ class ExtensionManager:
 
         for category, exts in extensions_config["extensions"].items():
             if type(category) != str or type(exts) != list:
-                print("[ExtensionManager.validate] extensions_config category and exts(List[Dict[str, str]]) must be a string, list.")
+                print(
+                    "[ExtensionManager.validate] extensions_config category and exts(List[Dict[str, str]]) must be a string, list.")
                 print(type(category), type(exts))
                 return False
             for ext in exts:
                 if type(ext) != dict:
-                    print("[ExtensionManager.validate] extensions_config extension in exts(List[Dict[str, str]]) must be a dictionary.")
+                    print(
+                        "[ExtensionManager.validate] extensions_config extension in exts(List[Dict[str, str]]) must be a dictionary.")
                     print(type(ext))
                     return False
                 elif "ext_name" not in ext.keys() or "ext_file" not in ext.keys():
@@ -70,7 +72,8 @@ class ExtensionManager:
                     print(json.dumps(obj=ext))
                     return False
                 elif type(ext["ext_name"]) != str or type(ext["ext_file"]) != str:
-                    print("[ExtensionManager.validate] extensions_config keys (ext_name, ext_file) must contain string values.")
+                    print(
+                        "[ExtensionManager.validate] extensions_config keys (ext_name, ext_file) must contain string values.")
 
         return True
 
@@ -82,16 +85,15 @@ class ExtensionManager:
         :return:
         """
 
-        ext_category: str = options.pop("ext_category") if "ext_category" in options.keys() else ''
-        ext_name: str = options.pop("ext_name") if "ext_name" in options.keys() else ''
-        ext_dir: str = options.pop("ext_dir") if "ext_dir" in options.keys() else ''
+        ext_category, ext_name, ext_dir = self.parse_params(options)
 
-        bot.get_logger().error(
+        bot.get_logger().info(
             msg=f"[ExtensionManager.load_ext] loading extension with options :\n ext_category : {ext_category}, ext_name : {ext_name}, ext_dir : {ext_dir}"
         )
 
-        if ext_category is '' and ext_name is '' and ext_dir is '':
-            raise BadExtArguments("None of the arguments can be used in loading extension!", ext_category=ext_category, ext_name=ext_name, ext_dir=ext_dir)
+        if ext_category == '' and ext_name == '' and ext_dir == '':
+            raise BadExtArguments("None of the arguments can be used in loading extension!", ext_category=ext_category,
+                                  ext_name=ext_name, ext_dir=ext_dir)
 
         try:
             if ext_category != '' and ext_name != '':
@@ -100,7 +102,7 @@ class ExtensionManager:
                 bot.load_extension(ext_dir)
 
         except ExtensionNotFound as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.load_ext] Extension name `{ext_name}` not found in extension map. " \
                       f"Are you sure it is included in extension map?"
             else:
@@ -110,7 +112,7 @@ class ExtensionManager:
                 exc_info=e
             )
         except ExtensionAlreadyLoaded as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.load_ext] Extension `{ext_name}` is already loaded!"
             else:
                 msg = f"[ExtensionManager.load_ext] Extension in `{ext_dir}` is already loaded!"
@@ -119,7 +121,7 @@ class ExtensionManager:
                 exc_info=e
             )
         except NoEntryPointError as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.load_ext] Extension `{ext_name}` does not have setup() function!"
             else:
                 msg = f"[ExtensionManager.load_ext] Extension in `{ext_dir}` does not have setup() function!"
@@ -128,7 +130,7 @@ class ExtensionManager:
                 exc_info=e
             )
         except ExtensionFailed as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.load_ext] Extension `{ext_name}` raised Exception! Maybe in __init__ function?"
             else:
                 msg = f"[ExtensionManager.load_ext] Extension in `{ext_dir}` raised Exception! Maybe in __init__ function?"
@@ -147,9 +149,11 @@ class ExtensionManager:
 
     def _load_ext_by_name(self, bot, ext_category: str, ext_name: str):
         if ext_category not in self.ext_map.keys():
-            raise BadExtArguments(f"Given extension category `{ext_category}` does not exist!", ext_category=ext_category, ext_name=ext_name)
+            raise BadExtArguments(f"Given extension category `{ext_category}` does not exist!",
+                                  ext_category=ext_category, ext_name=ext_name)
         elif ext_name not in self.ext_map[ext_category].keys():
-            raise BadExtArguments(f"Given extension name `{ext_name}` does not exist in category `{ext_name}`!", ext_category=ext_category, ext_name=ext_name)
+            raise BadExtArguments(f"Given extension name `{ext_name}` does not exist in category `{ext_name}`!",
+                                  ext_category=ext_category, ext_name=ext_name)
 
         bot.load_extension(self.ext_map[ext_category][ext_name])
 
@@ -161,15 +165,13 @@ class ExtensionManager:
         :return:
         """
 
-        ext_category: str = options.pop("ext_category") if "ext_category" in options.keys() else None
-        ext_name: str = options.pop("ext_name") if "ext_name" in options.keys() else None
-        ext_dir: str = options.pop("ext_dir") if "ext_dir" in options.keys() else None
+        ext_category, ext_name, ext_dir = self.parse_params(options)
 
-        bot.get_logger().error(
+        bot.get_logger().info(
             msg=f"[ExtensionManager.unload_ext] unloading extension with options :\n ext_category : {ext_category}, ext_name : {ext_name}, ext_dir : {ext_dir}"
         )
 
-        if ext_category is None and ext_name is None and ext_dir is None:
+        if ext_category == '' and ext_name == '' and ext_dir == '':
             raise BadArgument("None of the arguments can be used in loading extension!")
 
         try:
@@ -179,7 +181,7 @@ class ExtensionManager:
                 bot.unload_extension(ext_dir)
 
         except ExtensionNotLoaded as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.unload_ext] Extension name `{ext_name}` is not loaded into bot. Are you sure it is already loaded?"
             else:
                 msg = f"[ExtensionManager.unload_ext] Extension in {ext_dir} is not loaded into bot. Are you sure it is already loaded?"
@@ -196,9 +198,11 @@ class ExtensionManager:
 
     def _unload_ext_by_name(self, bot, ext_category: str, ext_name: str):
         if ext_category not in self.ext_map.keys():
-            raise BadExtArguments(f"Given extension category `{ext_category}` does not exist!", ext_category=ext_category, ext_name=ext_name)
+            raise BadExtArguments(f"Given extension category `{ext_category}` does not exist!",
+                                  ext_category=ext_category, ext_name=ext_name)
         elif ext_name not in self.ext_map[ext_category].keys():
-            raise BadExtArguments(f"Given extension name `{ext_name}` does not exist in category `{ext_name}`!", ext_category=ext_category, ext_name=ext_name)
+            raise BadExtArguments(f"Given extension name `{ext_name}` does not exist in category `{ext_name}`!",
+                                  ext_category=ext_category, ext_name=ext_name)
 
         bot.unload_extension(self.ext_map[ext_category][ext_name])
 
@@ -210,15 +214,13 @@ class ExtensionManager:
         :return:
         """
 
-        ext_category: str = options.pop("ext_category") if "ext_category" in options.keys() else None
-        ext_name: str = options.pop("ext_name") if "ext_name" in options.keys() else None
-        ext_dir: str = options.pop("ext_dir") if "ext_dir" in options.keys() else None
+        ext_category, ext_name, ext_dir = self.parse_params(options)
 
-        bot.get_logger().error(
+        bot.get_logger().info(
             msg=f"[ExtensionManager.reload_ext] reloading extension with options :\n ext_category : {ext_category}, ext_name : {ext_name}, ext_dir : {ext_dir}"
         )
 
-        if ext_category is None and ext_name is None and ext_dir is None:
+        if ext_category == '' and ext_name == '' and ext_dir == '':
             raise BadArgument("None of the arguments can be used in loading extension!")
 
         try:
@@ -228,7 +230,7 @@ class ExtensionManager:
                 bot.reload_extension(ext_dir)
 
         except ExtensionNotLoaded as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.reload_ext] Extension name `{ext_name}` is not loaded into bot. Are you sure it is already loaded?"
             else:
                 msg = f"[ExtensionManager.reload_ext] Extension in {ext_dir} is not loaded into bot. Are you sure it is already loaded?"
@@ -238,7 +240,7 @@ class ExtensionManager:
             )
 
         except ExtensionNotFound as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.reload_ext] Extension name `{ext_name}` not found in extension map. " \
                       f"Are you sure it is included in extension map?"
             else:
@@ -248,7 +250,7 @@ class ExtensionManager:
                 exc_info=e
             )
         except NoEntryPointError as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.reload_ext] Extension `{ext_name}` does not have setup() function!"
             else:
                 msg = f"[ExtensionManager.reload_ext] Extension in `{ext_dir}` does not have setup() function!"
@@ -257,7 +259,7 @@ class ExtensionManager:
                 exc_info=e
             )
         except ExtensionFailed as e:
-            if ext_name is not None:
+            if ext_name != '':
                 msg = f"[ExtensionManager.reload_ext] Extension `{ext_name}` raised Exception! Maybe in __init__ function?"
             else:
                 msg = f"[ExtensionManager.reload_ext] Extension in `{ext_dir}` raised Exception! Maybe in __init__ function?"
@@ -276,9 +278,11 @@ class ExtensionManager:
 
     def _reload_ext_by_name(self, bot, ext_category: str, ext_name: str):
         if ext_category not in self.ext_map.keys():
-            raise BadExtArguments(f"Given extension category `{ext_category}` does not exist!", ext_category=ext_category, ext_name=ext_name)
+            raise BadExtArguments(f"Given extension category `{ext_category}` does not exist!",
+                                  ext_category=ext_category, ext_name=ext_name)
         elif ext_name not in self.ext_map[ext_category].keys():
-            raise BadExtArguments(f"Given extension name `{ext_name}` does not exist in category `{ext_name}`!", ext_category=ext_category, ext_name=ext_name)
+            raise BadExtArguments(f"Given extension name `{ext_name}` does not exist in category `{ext_name}`!",
+                                  ext_category=ext_category, ext_name=ext_name)
 
         bot.reload_extension(self.ext_map[ext_category][ext_name])
 
@@ -287,8 +291,11 @@ class ExtensionManager:
             msg="[ExtensionManager.load_all] loading all extensions in map..."
         )
         for ext_category, exts in self.ext_map.items():
+            print("EXT CATEGORY :", ext_category)
             for ext_name, ext_path in exts.items():
-                self.load_ext(bot=bot, ext_dir=ext_path)
+                print("EXT NAME :", ext_name)
+                print("EXT PATH :", ext_path)
+                self.load_ext(bot=bot, ext_category=ext_category, ext_name=ext_name, ext_dir=ext_path)
         bot.get_logger().info(
             msg="[ExtensionManager.load_all] successfully loaded all extensions in map!"
         )
@@ -298,8 +305,15 @@ class ExtensionManager:
             msg="[ExtensionManager.unload_all] unloading all extensions in map..."
         )
         for ext_category, exts in self.ext_map.items():
+            print("EXT CATEGORY :", ext_category)
             for ext_name, ext_path in exts.items():
-                self.unload_ext(bot=bot, ext_dir=ext_path)
+                print("EXT NAME :", ext_name)
+                print("EXT PATH :", ext_path)
+                if (ext_category == "Dev" and ext_name == "admin") or ext_path == "extensions.Dev.AdminExt":
+                    # To load extensions again, we should not unload "Dev.admin" extension
+                    # But for some excuses, we should let manual-unload for "Dev.admin" extension can be possible.
+                    continue
+                self.unload_ext(bot=bot, ext_category=ext_category, ext_name=ext_name, ext_dir=ext_path)
         bot.get_logger().info(
             msg="[ExtensionManager.unload_all] successfully unloaded all extensions in map!"
         )
@@ -309,8 +323,11 @@ class ExtensionManager:
             msg="[ExtensionManager.reload_all] reloading all extensions in map..."
         )
         for ext_category, exts in self.ext_map.items():
+            print("EXT CATEGORY :", ext_category)
             for ext_name, ext_path in exts.items():
-                self.reload_ext(bot=bot, ext_dir=ext_path)
+                print("EXT NAME :", ext_name)
+                print("EXT PATH :", ext_path)
+                self.reload_ext(bot=bot, ext_category=ext_category, ext_name=ext_name, ext_dir=ext_path)
         bot.get_logger().info(
             msg="[ExtensionManager.reload_all] successfully reloaded all extensions in map!"
         )
@@ -321,15 +338,16 @@ class ExtensionManager:
         :param dir: normal directory string to convert.
         :return: converted python module location string.
         """
-
         if dir.startswith('.'):
             dir = dir[1:]
-
         if dir.startswith('/'):
             dir = dir[1:]
-
         if ".py" in dir:
             dir = dir.replace(".py", '')
-
         return dir.replace('/', '.') if '/' in dir else dir
 
+    def parse_params(self, params: Dict[str, Any]) -> Tuple[Optional[str], Optional[str], Optional[str]]:
+        ext_category: str = params.pop("ext_category") if "ext_category" in params.keys() else ''
+        ext_name: str = params.pop("ext_name") if "ext_name" in params.keys() else ''
+        ext_dir: str = params.pop("ext_dir") if "ext_dir" in params.keys() else ''
+        return ext_category, ext_name, ext_dir
