ee1df303725d85cc75334acc9386ddfc47b7343c
Lapis0875 <lapis0875@gmail.com>
2020-08-28 01:22:31 +0900

    feat: Update utils/tools.py Create some functions to parse keyword argument from kwargs. Create NotNull exception to raise exception when value must not be nullable

--- utils/tools.py
+++ utils/tools.py
@@ -3,7 +3,7 @@ utils.tools
 
 """
 from argparse import ArgumentTypeError
-from typing import Dict
+from typing import Dict, Any, Tuple, Union, List, NoReturn
 
 
 def cast_to_bool(arg: str):
@@ -19,6 +19,45 @@ def cast_to_bool(arg: str):
     else:
         msg = f"Cannot cast an argument `{arg}` into boolean : Not a boolean indicator"
         raise ArgumentTypeError(msg)
+
+
+class NotNull(Exception):
+    def __init__(self, key: str, kwargs: Dict[str, Any]):
+        self.key = key
+        self.kwargs = kwargs
+
+    def __str__(self) -> str:
+        return f"key {self.key} in kwargs {self.kwargs} can not be null value (none)"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+def get_value_from_kwargs(key: str, kwargs: Dict[str, Any], notnull: bool = False) -> Union[Dict[str, Any], NoReturn]:
+    if key in kwargs.keys():
+        return kwargs.pop(key)
+    else:
+        if notnull:
+            raise NotNull(key=key, kwargs=kwargs)
+        else:
+            return None
+
+
+def parse_args_from_kwargs(key: Union[str, Tuple[str, ...]], kwargs: Dict[str, Any], default: Any = None) -> Any:
+    
+    if type(key) == str:
+        return kwargs.pop(key) if key in kwargs else default
+    elif type(key) == tuple:
+        results: List[Any] = []
+        for key_choice in key:
+            if key_choice in kwargs:
+                results.append(kwargs.pop(key_choice))
+
+        if len(results) > 1:
+            raise KeyError("[utils.parse_args_from_kwargs] Cannot parse two or more keys at once!"
+                           "Use this function for each single keys.")
+        else:
+            return results[0]
 
 
 def get_cog_name_in_ext(ext_map: Dict[str, Dict[str, str]], module_path: str) -> str:
