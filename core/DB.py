8ebf54beef72ffe8a3315c24ac126a4c55d79815
Lapis0875 <lapis0875@gmail.com>
2020-08-27 22:23:49 +0900

    feat: Rewrite Database classes. Using sqlalchemy`s ORM structure, rewrote all database to orm class mapping.

--- core/DB.py
+++ core/DB.py
@@ -1,102 +1,162 @@
+from datetime import datetime
 from typing import Tuple, Any, Dict, Union
-from abc import abstractmethod
-import discord, sqlalchemy
-
-Key = Union[str, int, bytes]
-
-
-class DatabaseWrapper:
-    SQL_QUERY = Tuple[str, Tuple[Any]]
-    __create_sql__: str = ""
-
-    def __init__(self, db_dir: str):
-        self.db_dir = db_dir
-
-    @classmethod
-    def defend_sql_injection(self, query: str, params: Tuple[Any]) -> SQL_QUERY:
-        """
-        Validate given_query to defend sql_injections
-        :param query: query to validate.
-        :param params: paramse needed in query
-        :return: validated and fixed query
-        """
-        pass
-
-    @abstractmethod
-    def connect(self):
-        """
-        connect to DB
-        """
-
-    @abstractmethod
-    def close(self):
-        """
-        close the DB connection
-        """
-        pass
-
-    @abstractmethod
-    def is_connected(self) -> bool:
-        """
-        Check if the DB is connected
-        :return: boolean value which indicates wheter DB is connected or not.
-        """
-        pass
-
-    @abstractmethod
-    def custom_query(self, query: str, params: Tuple[Any]) -> Any:
-        """
-        Post a custom query and retrieve response.
-        :param query: a query to process
-        :param params: a parameter used in query
-        :return:
-        """
-        pass
-
-    @abstractmethod
-    def create(self):
-        """
-        Create inital tables to use.
-        self.__create_sql__ is used to create DB contents.
-        """
-        pass
+import sqlalchemy
+from sqlalchemy import Column, INTEGER, String, DATETIME
+from sqlalchemy.ext.declarative import declarative_base
+from utils import get_value_from_kwargs
 
 
-class UserDB(DatabaseWrapper):
-    def get(self, user_id: int, **options: Dict[Key, Any]) -> Any:
-        """
-        Get user data
-        :param user_id:
-        :param options:
-        :return:
-        """
-        pass
-
-    def set(self, user_id: int, **datas: Dict[Key, Any]):
-        """
-        Set user data
-        :param user_id:
-        :param datas:
-        :return:
-        """
-        pass
+def defend_sql_injection(query: str, params: Tuple[Any, ...]) -> Tuple[str, Tuple[Any, ...]]:
+    """
+    Validate given_query to defend sql_injections
+    :param query: query to validate.
+    :param params: paramse needed in query
+    :return: validated and fixed query
+    """
+    pass
 
 
-class GuildDB(DatabaseWrapper):
-    def get(self, guild_id: int, **options: Dict[Key, Any]) -> Any:
-        """
-        Get guild data
-        :param user_id:
-        :param options:
-        :return:
-        """
-        pass
+# Define and create the table
+Base = declarative_base()
 
-    def set(self, user_id: int, **datas: Dict[Key, Any]):
-        """
-        Set guild data
-        :param user_id:
-        :param datas:
-        :return:
-        """
-        pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Basic Information
+    id = Column(INTEGER, primary_key=True, nullable=False)
+    name = Column(String, nullable=False)
+    level = Column(INTEGER, nullable=False)
+
+    # Economy
+    wallet = Column(INTEGER)
+
+    def __init__(self, name, level=0, wallet=0):
+        self.name = name
+        self.level = level
+
+    def __repr__(self):
+        return f"<User({self.name}, {self.level})>"
+
+
+"""
+Guild side tables
+"""
+
+
+class Guild(Base):
+    __tablename__ = "guilds"
+
+    # Basic Information
+    id = Column(INTEGER, primary_key=True)
+    name = Column(String, nullable=False)
+
+    # Guild-specific latte configuration
+    prefix = Column(String, nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"<Guild({self.id}{self.name})>"
+
+
+class WarnRecord(Base):
+    __tablename__ = "warns"
+
+    # Basic Information
+    id = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
+
+    # Guild Information
+    guild_id = Column(INTEGER, nullable=False)
+    guild_name = Column(String)
+
+    # User Information
+    user_id = Column(INTEGER, nullable=False)
+    user_name = Column(String)
+
+    # Warn information
+    reason = Column(String)
+    when = Column(DATETIME, nullable=False)
+
+    def __init__(self, guild_id: int, guild_name: str, user_id: int, user_name: str, when: datetime, reason: str):
+        # Guild Information
+        self.guild_id = guild_id
+        self.guild_name = guild_name
+
+        # User Information
+        self.user_id = user_id
+        self.user_name = user_name
+
+        # Warn information
+        self.when = when
+        self.reason = reason
+
+    def __repr__(self):
+        return f"<WarnRecord({self.id}, {self.guild_id}, {self.user_id}, {self.when}, {self.reason})>"
+
+
+class MuteRecord(Base):
+    __tablename__ = "mutes"
+
+    # Basic Information
+    id = Column(INTEGER, primary_key=True, nullable=False, autoincrement=True)
+
+    # Guild Information
+    guild_id = Column(INTEGER, nullable=False)
+    guild_name = Column(String)
+
+    # User Information
+    user_id = Column(INTEGER, nullable=False)
+    user_name = Column(String)
+
+    # Mute information
+    reason = Column(String, default="UNDEFINED")
+    start = Column(DATETIME, nullable=False)
+    end = Column(DATETIME, nullable=False)
+
+    def __init__(self, guild_id: int, guild_name: str, user_id: int, user_name: str, start: datetime, end: datetime, reason: str):
+        # Guild Information
+        self.guild_id = guild_id
+        self.guild_name = guild_name
+
+        # User Information
+        self.user_id = user_id
+        self.user_name = user_name
+
+        # Mute information
+        self.start = start
+        self.end = end
+        self.reason = reason
+
+    def __repr__(self):
+        return f"<MuteRecord({self.id},{self.guild_id},{self.user_id},{self.start}~{self.end},{self.reason})>"
+
+
+class DBWrapper:
+    users_engine = None
+    guilds_engine = None
+
+    def __init__(self, **attrs):
+        self.id = get_value_from_kwargs("id", attrs, notnull=True)
+        self.pw = get_value_from_kwargs("pw", attrs, notnull=True)
+        self.host = get_value_from_kwargs("host", attrs, notnull=True)
+        self.port = get_value_from_kwargs("port", attrs, notnull=True)
+
+    def create_engine(self):
+        # Define the MySQL engine using MySQL Connector/Python
+        base_uri = f"mysql+mysqlconnector://{self.id}:{self.pw}@{self.host}:{self.port}"
+
+        # guilds database(schema)
+        self.guilds_engine = sqlalchemy.create_engine(
+            f"{base_uri}/latte_guild",
+            echo=True,
+            convert_unicode=False
+        )
+
+        # users database(schema)
+        self.users_engine = sqlalchemy.create_engine(
+            f"{base_uri}/latte_user",
+            echo=True,
+            convert_unicode=False
+        )
