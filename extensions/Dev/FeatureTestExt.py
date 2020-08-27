bd5eb776da3a6c61cd40ef635792c1c816a45fd5
Lapis0875 <lapis0875@gmail.com>
2020-08-27 15:17:12 +0900

    [update] Update FeatureTestExt.py Removed some commands that are no longer needed.

--- extensions/Dev/FeatureTestExt.py
+++ extensions/Dev/FeatureTestExt.py
@@ -6,28 +6,6 @@ class TestCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(
-        name="tsi",
-        description="VoiceRegion enum does not have `south-korea` region, so we made PR to add it."
-    )
-    async def testserverinfo(self, ctx: commands.Context):
-        await ctx.send(content=f"> Your server region is : {ctx.guild.region}. type : {type(ctx.guild.region)}")
-
-    @commands.command(
-        name="hellothisisverification",
-        description="Verification command for adding Latte on koreanbots.",
-        help="`l; hellothisisverification` to use."
-    )
-    async def koreanbots_verfy(self, ctx: commands.Context):
-        await ctx.send(content="")
-
-    @commands.command(
-        name="개발자바보",
-        description="유저 HOREON이 많은 양의 테스트를 수행해준 노고에 대한 보답으로 건의받은 명령어"
-    )
-    async def lapisbabo(self, ctx: commands.Context):
-        await ctx.send(content="라피스 바보")
-
 
 def setup(bot):
     cog = TestCog(bot)
