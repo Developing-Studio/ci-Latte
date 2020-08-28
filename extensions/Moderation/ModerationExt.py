14cd616511df779bf62314fd7bd2d4d95e84b54c
Lapis0875 <lapis0875@gmail.com>
2020-08-29 01:54:05 +0900

    feat: Update extensions/Moderation/ModerationExt.py Now moderation commands send it`s result with embed.

--- extensions/Moderation/ModerationExt.py
+++ extensions/Moderation/ModerationExt.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from discord.ext import commands
 import discord
 from core import Latte
@@ -27,6 +29,25 @@ class ModerationCog(commands.Cog):
     )
     async def kick(self, ctx: commands.Context, target_member: discord.Member, *, reason: str = ""):
         await target_member.kick(reason=reason)
+        return await ctx.send(
+            embed=await EmbedFactory(
+                title="[ 관리 ] 멤버를 추방했습니다!",
+                footer=EmbedFactory.get_command_caller(ctx.author),
+                color=EmbedFactory.default_color,
+                fields=[
+                    {
+                        "name": "추방한 멤버",
+                        "value": EmbedFactory.get_user_info(target_member),
+                        "inline": False
+                    },
+                    {
+                        "name": "추방 사유",
+                        "value": reason,
+                        "inline": False
+                    }
+                ]
+            ).build()
+        )
 
     @moderation.command(
         name="ban",
@@ -36,6 +57,25 @@ class ModerationCog(commands.Cog):
     )
     async def ban(self, ctx: commands.Context, target_member: discord.Member, *, reason: str = ""):
         await target_member.ban(reason=reason)
+        return await ctx.send(
+            embed=await EmbedFactory(
+                title="[ 관리 ] 멤버를 차단했습니다!",
+                footer=EmbedFactory.get_command_caller(ctx.author),
+                color=EmbedFactory.default_color,
+                fields=[
+                    {
+                        "name": "차단한 멤버",
+                        "value": EmbedFactory.get_user_info(target_member),
+                        "inline": False
+                    },
+                    {
+                        "name": "차단 사유",
+                        "value": reason,
+                        "inline": False
+                    }
+                ]
+            ).build()
+        )
 
     @moderation.command(
         name="unban",
@@ -44,13 +84,34 @@ class ModerationCog(commands.Cog):
         help="`l; moderation unban @member (reason)` to use."
     )
     async def unban(self, ctx: commands.Context, target_id: int, *, reason: str = ""):
-        target_ban_entry: discord.guild.BanEntry = discord.utils.find(lambda be: be.user.id == id, await ctx.guild.bans())
+        target_ban_entry: discord.guild.BanEntry = discord.utils.find(lambda be: be.user.id == target_id, await ctx.guild.bans())
+        if target_ban_entry is None:
+            return await ctx.send("해당 유저는 차단되지 않았습니다!")
         await ctx.guild.unban(target_ban_entry.user, reason=reason)
-        unban_embed = EmbedFactory
-        unban_embed = discord.Embed(title="**언밴**", description=f"*{target_ban_entry.user.mention} 님이 밴 해제 처리되었습니다.*")
-        unban_embed.add_field(name="**사유**", value=f"*{reason}*", inline=False)
-
-        await ctx.send(embed=unban_embed)
+        return await ctx.send(
+            embed=await EmbedFactory(
+                title="[ 관리 ] 멤버를 차단 해제했습니다!",
+                footer=EmbedFactory.get_command_caller(ctx.author),
+                color=EmbedFactory.default_color,
+                fields=[
+                    {
+                        "name": "차단한 멤버",
+                        "value": EmbedFactory.get_user_info(target_ban_entry.user),
+                        "inline": False
+                    },
+                    {
+                        "name": "차단 해제 사유",
+                        "value": reason,
+                        "inline": False
+                    },
+                    {
+                        "name": "이 유저가 차단되었던 사유",
+                        "value": target_ban_entry.reason,
+                        "inline": False
+                    }
+                ]
+            ).build()
+        )
 
     @moderation.command(
         name="clearchat",
@@ -61,7 +122,14 @@ class ModerationCog(commands.Cog):
     async def clearchat(self, ctx: commands.Context, amount: int = 5):
         if amount < 1:
             return await ctx.send(f"{amount} 는 너무 적습니다!")
-        await ctx.channel.purge(limit=amount+1)     # Add 1 to :param amout: to delete command message.
+        deleted: List[discord.Message] = await ctx.channel.purge(limit=amount+1)     # Add 1 to :param amout: to delete command message.
+        return await ctx.send(
+            embed=EmbedFactory.COMMAND_LOG_EMBED(
+                title="메세지 청소 결과",
+                description=f"{len(deleted)-1}개의 메세지를 청소했습니다!",
+                user=ctx.author
+            )
+        )
 
 
 def setup(bot: Latte):
